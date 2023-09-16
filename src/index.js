713d7fc9b1528abe4d090ad9def709cf05a209de
devanshu <devanshusenp@gmail.com>
2023-09-16 17:39:48 -0500

    added redirecting routes to projects

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import RedirectData from "./RedirectData.json";
+
+const RedirectTo = ({ to }) => {
+  window.location.href = to;
+  return null;
+};
+
+const pathname = window.location.pathname;
+const isRedirectPath = RedirectData.some((item) => item.name === pathname);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {isRedirectPath ? (
+      <RedirectTo
+        to={RedirectData.find((item) => item.name === pathname).link}
+      />
+    ) : (
+      <App />
+    )}
   </React.StrictMode>,
   document.getElementById("root")
 );
